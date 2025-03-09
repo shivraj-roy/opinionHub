9142b18f66ad14263d1924a89e9ab2048d97e4ab
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-09 20:10:49 +0530

    ☑︎ add: implement opinion submission form with validation...

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,24 +1,82 @@
+import { useActionState } from "react";
+
 export function NewOpinion() {
+   const shareOpinionAction = (prev, formData) => {
+      const userName = formData.get("userName");
+      const title = formData.get("title");
+      const body = formData.get("body");
+      const data = { userName, title, body };
+
+      let error = [];
+      if (userName.trim().length < 3) {
+         error.push("Name must be at least 3 characters long.");
+      }
+      if (title.trim().length < 3) {
+         error.push("Title must be at least 3 characters long.");
+      }
+      if (body.trim().length < 10) {
+         error.push("Opinion must be at least 10 characters long.");
+      }
+      if (error.length > 0) {
+         return { error, enteredValue: data };
+      }
+      // fetch("/api/opinions", {
+      //    method: "POST",
+      //    headers: { "Content-Type": "application/json" },
+      //    body: JSON.stringify(data),
+      // }).then(() => {
+      //    window.location.reload();
+      // });
+      console.log(data);
+      return { error: [] };
+   };
+   const [formState, formAction] = useActionState(shareOpinionAction, {
+      error: [],
+      enteredValue: {},
+   });
+
    return (
       <div id="new-opinion">
          <h2>Share your opinion!</h2>
-         <form>
+         <form action={formAction}>
             <div className="control-row">
                <p className="control">
                   <label htmlFor="userName">Your Name</label>
-                  <input type="text" id="userName" name="userName" />
+                  <input
+                     type="text"
+                     id="userName"
+                     name="userName"
+                     defaultValue={formState.enteredValue?.userName}
+                  />
                </p>
-
                <p className="control">
                   <label htmlFor="title">Title</label>
-                  <input type="text" id="title" name="title" />
+                  <input
+                     type="text"
+                     id="title"
+                     name="title"
+                     defaultValue={formState.enteredValue?.title}
+                  />
                </p>
             </div>
             <p className="control">
                <label htmlFor="body">Your Opinion</label>
-               <textarea id="body" name="body" rows={5}></textarea>
+               <textarea
+                  id="body"
+                  name="body"
+                  rows={5}
+                  defaultValue={formState.enteredValue?.body}
+               ></textarea>
             </p>
-
+            // TODO: Show the error underneath the field not in separate list
+            {formState.error.length > 0 && (
+               <ul className="errors">
+                  <p>Errors:</p>
+                  {formState.error.map((e, i) => (
+                     <li key={i}>{e}</li>
+                  ))}
+               </ul>
+            )}
             <p className="actions">
                <button type="submit">Submit</button>
             </p>
